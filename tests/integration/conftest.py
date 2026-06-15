c133a155bdcd5e317e5d6337c47d957e90b6bc57
Trong Nhan Mai <trongnhan.mai@canonical.com>
2026-06-15 10:21:54 +1000

    init from operator example charm

---
+++ tests/integration/conftest.py
@@ -0,0 +1,36 @@
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# The integration tests use the Jubilant library and the pytest-jubilant plugin.
+# See https://documentation.ubuntu.com/ops/latest/howto/write-integration-tests-for-a-charm/
+
+import os
+import pathlib
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def charm():
+    """Return the path of the charm under test."""
+    charm = os.environ.get("CHARM_PATH")
+    if not charm:
+        charm_dir = pathlib.Path()  # Assume the current working directory is the charm root.
+        charms = list(charm_dir.glob("*.charm"))
+        assert charms, f"No charms were found in {charm_dir.absolute()}"
+        assert len(charms) == 1, f"Found more than one charm {charms}"
+        charm = charms[0]
+    path = pathlib.Path(charm).resolve()
+    assert path.is_file(), f"{path} is not a file"
+    return path
---
+++ tests/integration/test_charm.py
@@ -0,0 +1,41 @@
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# The integration tests use the Jubilant library and the pytest-jubilant plugin.
+# See https://documentation.ubuntu.com/ops/latest/howto/write-integration-tests-for-a-charm/
+#
+# pytest-jubilant provides a module-scoped `juju` fixture that creates a temporary Juju model.
+# The `charm` fixture is defined in conftest.py.
+
+import logging
+import pathlib
+
+import jubilant
+import pytest
+import yaml
+
+logger = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(pathlib.Path("charmcraft.yaml").read_text())
+APP_NAME = METADATA["name"]
+
+
+@pytest.mark.juju_setup
+def test_deploy(charm: pathlib.Path, juju: jubilant.Juju):
+    """Deploy the charm under test."""
+    resources = {
+        "demo-server-image": METADATA["resources"]["demo-server-image"]["upstream-source"]
+    }
+    juju.deploy(charm, app=APP_NAME, resources=resources)
+    juju.wait(jubilant.all_active)
